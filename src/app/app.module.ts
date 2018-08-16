de43e1448e379e6c7a170ade2dfbb664fa306969
sgokulnath <sgokulnath@kaartech.com>
2018-08-16 14:54:16 +0530

    VSTS Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MatSelectModule, MatButtonModule, MatSelect } from '@angular/material'; 
+import { DropdownComponent } from './dropdown/dropdown.component';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
