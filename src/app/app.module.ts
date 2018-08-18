00666d5818272b326ad9b1497b6aa129a8912d67
sgokulnath <sgokulnath@kaartech.com>
2018-08-18 15:49:29 +0530

    Week 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { MatSelectModule, MatButtonModule, MatSelect } from '@angular/material'; 
+import { MatSelectModule, MatButtonModule, MatListModule, MatToolbarModule, MatCardModule, MatGridListModule} from '@angular/material'; 
 import { DropdownComponent } from './dropdown/dropdown.component';
 import 'hammerjs';
-
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DropdownComponent
+    DropdownComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
